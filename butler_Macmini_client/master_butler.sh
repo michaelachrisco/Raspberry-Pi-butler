87b137eb52cfb1b7c50eba38b14c60669ba2843b
michael chrisco <michaelachrisco@gmail.com>
2012-10-12 15:00:45 -0700

    Update butler_Macmini_client/master_butler.sh

--- butler_Macmini_client/master_butler.sh
+++ butler_Macmini_client/master_butler.sh
@@ -1,2 +1,3 @@
+#If the curent time is between 6am and 10pm, do the operation. You can also set up a cron job if needed
 if [ "`date "+%H" | awk '{print $1}'`" -le "21" -a  "`date "+%H" | awk '{print $1}'`" -ge "6" ] ; then sh butler.sh; fi;
 exit 0;
